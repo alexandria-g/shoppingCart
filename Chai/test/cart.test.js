f491fd1883473b9758861f58af0a109811e51507
Alexandria G <alexandriag.801@gmail.com>
2023-08-24 01:13:54 +1000

    Initial Shopping Cart app

--- Chai/test/cart.test.js
+++ Chai/test/cart.test.js
@@ -3,39 +3,39 @@ const expect = require('chai').expect;
 
 describe('Testing eCommerce functions', function() {
         //setting instances of products
-        let apple = new Product("apple", 4.95);
-        let orange = new Product("orange", 3.99);
+        let laptop = new Product("laptop", 399.99);
+        let phone = new Product("phone", 895.85);
 
         //Test case 1
-    it('1. Adding 2 apples, 1 orange and checking total price of cart', function(done) {
+    it('1. Adding 2 laptops, 1 phone and checking total price of cart', function(done) {
         //creating new instance of the shopping cart    
         let cart = new ShoppingCart()
 
         //adding products to the cart
-        cart.add(apple)
-        cart.add(apple)
-        cart.add(orange)
+        cart.add(laptop)
+        cart.add(laptop)
+        cart.add(phone)
 
         //asserting the total price of products in the cart is as expected
-        expect(cart.getTotalPrice()).to.equal(13.89);
+        expect(cart.getTotalPrice()).to.equal(1695.83);
         done();
     });
 
     //Test case 2
-    it('2. Adding 3 apples, removing 1 apple and checking total price of cart', function(done) {
+    it('2. Adding 3 laptops, removing 1 laptop and checking total price of cart', function(done) {
         //creating new instance of the shopping cart
         let cart2 = new ShoppingCart()
 
         //adding products to the cart
-        cart2.add(apple)
-        cart2.add(apple)
-        cart2.add(apple)
+        cart2.add(laptop)
+        cart2.add(laptop)
+        cart2.add(laptop)
 
-        //removing one apple product from the cart
-        cart2.remove("apple")
+        //removing one laptop product from the cart
+        cart2.remove("laptop")
 
         //asserting the total price of products in the cart is as expected
-        expect(cart2.getTotalPrice()).to.equal(9.9);
+        expect(cart2.getTotalPrice()).to.equal(799.98);
         done();
     });
 
